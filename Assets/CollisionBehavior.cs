ff64441b0c2709c0a1e4f1ab3c48b888b312842a
Robert Desjardins <roberttdesjardins@gmail.com>
2018-04-07 16:13:36 -0700

    Finished unity tutorial 1

---
+++ Assets/CollisionBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionBehavior : MonoBehaviour {
+
+	// 1
+	public bool collided;
+
+	//2
+	void OnCollisionEnter(Collision collision) {
+		if (!collided) {
+			collided = true;
+			// 3
+			GameObject clown = gameObject.transform.parent.gameObject;
+			Animator clownAnimator = clown.GetComponent<Animator> ();
+			// 4
+			clownAnimator.SetTrigger ("hit");
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
